07488d677fa5d94c06e91b94a855ca22d1b0b85f
Student KhushiSingh <Student@khushisingh.in>
2025-12-26 18:39:28 +0530

    Add validation in sign up form and fixed the navbar

--- script.js
+++ script.js
@@ -21,3 +21,35 @@ window.addEventListener("click", (e) => {
     signupPopup.style.display = "none";
   }
 });
+
+// Form Validation and Submission
+// document
+//   .getElementById("contact-form")
+//   .addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     const name = document.getElementById("name").value;
+//     const email = document.getElementById("email").value;
+//     const message = document.getElementById("message").value;
+
+//     if (name && email && message) {
+//       alert("Message Sent! We will get back to you soon.");
+//       // Here you would typically handle form submission via AJAX
+//       this.reset(); // Reset the form fields after submission
+//     } else {
+//       alert("Please fill in all fields.");
+//     }
+//   });
+
+document.getElementById("sign-up-form").addEventListener("click", function (e) {
+    e.preventDefault();
+    const Name = document.getElementById("userName").value;
+    const Email = document.getElementById("userEmail").value;
+    const Password = document.getElementById("userPassword").value;
+
+    if (Name && Email && Password) {
+      alert(`Sign Up Successfully! Thankyou ${Name} for Sign Up.`);
+      this.reset(); 
+    } else {
+      alert(`${Name} Please fill in all fields.`);
+    }
+  });
